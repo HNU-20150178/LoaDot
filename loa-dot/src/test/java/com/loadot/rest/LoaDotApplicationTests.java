11236cb059101aa2a67ee577797a9aff17a67213
HNU-20150178 <yhc9632@gmail.com>
2026-02-09 17:01:25 +0900

    Initial Commit: Spring Boot Project for LoaDot

---
+++ loa-dot/src/test/java/com/loadot/rest/LoaDotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadot.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaDotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
